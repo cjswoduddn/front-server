20c739d951ce69ccfee6cfd81305e9dd2d48a58a
R _ R <exit19093@gmail.com>
2020-12-29 17:14:55 +0900

    seperate factory dir

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
 import './nav.css';
-import BigImage from './images/big.png';
-import SmallImage from './images/small.png';
+import BigImage from '../factory/images/big.png';
+import SmallImage from '../factory/images/small.png';
 import NavItem from './NavItem';
 import { Link } from 'react-router-dom';
 
