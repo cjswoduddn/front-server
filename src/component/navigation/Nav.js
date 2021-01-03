35991a0978da2fccf8dd5868aeff594e7e4057a3
R _ R <exit19093@gmail.com>
2021-01-03 22:47:20 +0900

    nav shrink animation

--- src/component/navigation/Nav.js
+++ src/component/navigation/Nav.js
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
 import './nav.css';
-import BigImage from '../../factory/images/big.png';
-import SmallImage from '../../factory/images/small.png';
+import logo from '../../factory/images/logotem2.png';
 import NavItem from './NavItem';
 import { Link } from 'react-router-dom';
 
 const Nav = () =>{
-
-  const [mode, setMode] = useState("nav-top");
-  const [img, setImg] = useState(BigImage);
+  const [mode, setMode] = useState("nav-init");
 
   useEffect(()=>{
     function handleMode(){
       if(window.scrollY > 0){
-        setMode("nav-bottom");
-        setImg(SmallImage);
+        setMode("nav-shrink");
       }else{
-        setMode("nav-top");
-        setImg(BigImage);
+        setMode("nav-init");
       }
     }
     window.addEventListener("scroll", handleMode);
@@ -30,16 +25,14 @@ const Nav = () =>{
 
   return(
     <Grid container justify="center" className={mode}>
-      <Grid container xs="11" className="item">
-
+      <Grid item xs={11}>
         <Grid item xs="4" className="item">
-          <Link to="/"><img src={img}/></Link>
+          <Link to="/"><img className={mode} src={logo}/></Link>
         </Grid>
 
         <Grid item xs="8" className="item">
           <NavItem/>
         </Grid>
-        
       </Grid>
     </Grid>
   )
