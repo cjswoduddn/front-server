cc0c8440d018c7660755db162a5c946fe2cf07d1
cjswo-duddn <cjswoudddn@naver.com>
2021-01-04 10:33:48 +0900

    nav-bar-first-commit

--- src/component/navigation/NavItem.js
+++ src/component/navigation/NavItem.js
@@ -6,9 +6,9 @@ const NavItem = () =>{
   return (
     <nav>
       <ul className="nav-ul">
-        <li className="nav-li"><Link to="/accounts" className="nav-link">Sign In</Link></li>
-        <li className="nav-li"><Link to="/head_hunt" className="nav-link">Head Hunting</Link></li>
-        <li className="nav-li"><Link to="/template" className="nav-link">Template</Link></li>
+        <li className="nav-li"><Link to="/head_hunt" className="nav-link">검색</Link></li>
+        <li className="nav-li"><Link to="/template" className="nav-link">템플릿</Link></li>
+        <li className="nav-li"><Link to="/accounts" className="nav-link">로그인</Link></li>
       </ul>
     </nav>
   )
