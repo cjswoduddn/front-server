2bacdf000472df122b8f66b7ee3763ef2921c0df
R _ R <exit19093@gmail.com>
2021-01-05 14:54:35 +0900

    nav link changed

--- src/component/navigation/NavItem.js
+++ src/component/navigation/NavItem.js
@@ -5,11 +5,11 @@ import './Nav.css';
 const NavItem = () =>{
   return (
     <nav>
-      <ul className="nav-ul">
-        <Link to="/accounts" className="nav-link"><li onClick className="nav-li">sign in</li></Link>
-        <Link to="/head_hunt" className="nav-link"><li className="nav-li">head hunting</li></Link>
-        <Link to="/template" className="nav-link"><li className="nav-li">template</li></Link>
-      </ul>
+        <ul className="nav-ul">
+            <Link to="/head_hunt" className="nav-link"><li className="nav-li">head hunting</li></Link>
+            <Link to="/template" className="nav-link"><li className="nav-li">template</li></Link>
+            <Link to="/sign_in" className="nav-link"><li onClick className="nav-li">sign in</li></Link>
+        </ul>
     </nav>
   )
 }
--- src/component/signin/SignIn.js
+++ src/component/signin/SignIn.js
@@ -1,8 +1,11 @@
 import React from 'react';
+// import back3 from "../../factory/images/home_back1.gif";
+import signin_back from '../../factory/images/signin_back.jpg';
 
 const SignIn = () =>{
   return (
-    <div style={{backgroundColor: "green", height: '100vh', marginTop: '-7.1%'}}>
+    <div style={{backgroundImage: `url(${signin_back})`, height: '100vh'}}>
+
     </div>
   );
 }
