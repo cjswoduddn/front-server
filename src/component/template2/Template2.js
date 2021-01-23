11a3ee0a3184064472928255754b0dbe7a309d9d
cjswo-duddn <cjswoudddn@naver.com>
2021-01-23 12:27:34 +0900

    2021 01 23 최신화 사항
    1. multipart form data처리를 끝냈다.
    이제 데이터 개수와 종류에 상관없이
    backend api가 처리할 수 있는 전송 플렛폼을 만들었다.
    
    2. 여전히 너무 허전한 건 어쩔 수 없다.
    결국 템플릿 양식을 업그레이드 하기로 결정.. ㅋ
    
    3. 중첩 라우팅을 적용한 템플릿을 만들자
    aboutme resume project를 등록할 수 있는 페이지를 만들자

--- src/component/template2/Template2.js
+++ src/component/template2/Template2.js
@@ -1,19 +1,49 @@
 import React, {useState} from 'react';
-import {Grid} from '@material-ui/core';
+import {useForm} from 'react-hook-form';
+import {Grid, Button} from '@material-ui/core';
 import "./Template2.css";
+import axios from 'axios';
+
+const TEMPLATE_BASE_URL = 'http://localhost:8080/portfolio';
 
 const Template2 = () =>{
 
   const [thumbnail, setThumbnail] = useState({file: '', previewUrl: ''})
+  const {register, handleSubmit, watch, errors} = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+    const form = new FormData();
+    for (const [key, value] of Object.entries(data)) {
+      if(typeof value === 'object'){
+        form.append(key, value[0]);
+      }
+      else{
+        form.append(key, value);
+      }
+    }
+    axios({
+      method: 'post',
+      headers:{
+        'Content-Type': `multipart/form-data`
+      },
+      url: TEMPLATE_BASE_URL,
+      data: form
+    })
+    .then((res)=>{
+      console.log(res.body);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }
 
   const changeThumbnails = (e) =>{
     e.preventDefault();
-    console.log(e);
 
     let reader = new FileReader();
     let filelist = e.target.files;
     if(filelist === null || filelist.length === 0) return;
-    console.log("hello")
     let file = filelist[0];
     reader.onloadend = () => {
       setThumbnail({
@@ -25,29 +55,60 @@ const Template2 = () =>{
   }
 
   return (
-    <Grid container className="template2-container">
-      <Grid item xs="5" className="template2-left-item">
-        <Grid container xs="7" className="template2-left-item-profile" justify="center">
-          <label className="template2-left-item-profile-thumbnail-label" 
-            for="thumbnail"
-            style={{backgroundImage: `url(${thumbnail.previewUrl})`}}>
-            IMG
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="hidden" name="dtype" value="sampleportfolio" ref={register}/>
+      <Grid container className="template2-container">
+        <Grid item xs="5" className="template2-left-item">
+          <Grid container xs="7" className="template2-left-item-profile" justify="center">
+            <label className="template2-left-item-profile-thumbnail-label" 
+              for="thumbnail"
+              style={{backgroundImage: `url(${thumbnail.previewUrl})`}}>
+              IMG
+              <input
+                id="thumbnail"
+                accept="image/*" type="file"
+                className="template2-left-item-profile-thumbnail"
+                onChange={changeThumbnails}
+                name="thumbnail"
+                ref={register}
+              />
+            </label>
             <input
-              id="thumbnail"
-              accept="image/*" type="file"
-              className="template2-left-item-profile-thumbnail"
-              onChange={changeThumbnails}
+              type="text"
+              className="template2-left-item-profile-name" 
+              placeholder="NAME"
+              name="name"
+              ref={register}
             />
-          </label>
-          <input type="text" className="template2-left-item-profile-name" placeholder="NAME"></input>
+          </Grid>
+        </Grid>
+        <Grid item xs="7" className="template2-right-item">
+          <input 
+            type="text" 
+            className="template2-right-item-title" 
+            placeholder="TITLE"
+            name="title"
+            ref={register}
+          />
+          <input 
+            type="text" 
+            className="template2-right-item-skill" 
+            placeholder="skill"
+            name="skill"
+            ref={register}
+          />
+          <textarea 
+            className="template2-right-item-intro" 
+            placeholder="INTRO"
+            name="intro"
+            ref={register}
+            />
         </Grid>
       </Grid>
-      <Grid item xs="7" className="template2-right-item">
-        <input type="text" className="template2-right-item-title" placeholder="TITLE"></input>
-        <input type="text" className="template2-right-item-skill" placeholder="skill"></input>
-        <textarea className="template2-right-item-intro" placeholder="INTRO"></textarea>
+      <Grid container justify="center">
+        <Button type="submit" variant="outlined" size="large">SUBMIT</Button>
       </Grid>
-    </Grid>
+    </form>
   );
 }
 
